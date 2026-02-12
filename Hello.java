5dfe1a70e5bf996d1f3a1900982d70f87bedd6e6
maxf447 <maxfrank@mtu.edu>
2026-02-12 17:13:24 -0500

    Update Hello.java (oops we broke it)

--- Hello.java
+++ Hello.java
@@ -1,7 +1,6 @@
 class Hello {
     public static void main(String[] args) {
-        String s = "Hello from GitHub Actions";
-        s = null;
+        String s = "Hello from GitHub Action
         System.out.println(s); 
     }
 }
