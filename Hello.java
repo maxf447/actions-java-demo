bbc02029ef8dcc799c701ff491e9295efa801368
maxf447 <maxfrank@mtu.edu>
2026-02-12 17:14:38 -0500

    Fixed missing characters

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 class Hello {
     public static void main(String[] args) {
-        String s = "Hello from GitHub Action
+        String s = "Hello from GitHub Actions!!!!";
         System.out.println(s); 
     }
 }
